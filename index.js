951157bedb5a1611e5d898623207df6fc726b7a4
Oksana Cook <oksanacook@outlook.com>
2022-03-18 14:42:22 +1100

    Completed assignment

--- index.js
+++ index.js
@@ -3,21 +3,11 @@
 // let newElement = document.createElement('input')
 // document.body.appendChild(newElement)
 
-const ul = document.createElement("ul");
 
-for (let i = 0; i < 3; i++) {
-  const li = document.createElement("li");
-  li.textContent = (i + 1).toString();
-  ul.append(li);
-}
+var el = document.getElementById("main");
+el.remove()
 
-element.append(ul);
+let newHeader = document.createElement('h1')
+newHeader.setAttribute("id", "victory")
 
-
-// var el = document.getElementById("main");
-// el.remove()
-
-// let newHeader = document.createElement('h1')
-// newHeader.setAttribute("id", "victory")
-
-// newHeader.innerHTML = "Oksana is the champion";
+newHeader.innerHTML = "Oksana is the champion";
