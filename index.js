3c210f95a01920662db03f3dee3cf3a47de20831
Oksana Cook <oksanacook@outlook.com>
2022-03-18 14:35:55 +1100

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your code here!
+
+// let newElement = document.createElement('input')
+// document.body.appendChild(newElement)
+
+const ul = document.createElement("ul");
+
+for (let i = 0; i < 3; i++) {
+  const li = document.createElement("li");
+  li.textContent = (i + 1).toString();
+  ul.append(li);
+}
+
+element.append(ul);
+
+
+// var el = document.getElementById("main");
+// el.remove()
+
+// let newHeader = document.createElement('h1')
+// newHeader.setAttribute("id", "victory")
+
+// newHeader.innerHTML = "Oksana is the champion";
